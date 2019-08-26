21a15503b461929fa247186c00c50e7abfdcfa31
Email Update <haojinzhao@yahoo.com>
2019-08-26 11:11:14 -0500

    add creating news in db

--- models/News.js
+++ models/News.js
@@ -2,22 +2,22 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var NewsSchema = new Schema ({
-    headline: {
-        type: String,
-        required: true
-    },
-    summary: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true
-    },
-    note:{
-        type: Schema.Types.ObjectId,
-        ref: "Note"
-    }
+  headline: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  note:{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 });
 
 var News = mongoose.model("News",NewsSchema);
--- routes/router.js
+++ routes/router.js
@@ -1,3 +1,62 @@
 module.exports = function (app, db){
-  console.log("route",app,db);
+ 
+  var axios = require("axios");
+  var cheerio = require("cheerio");
+
+  // a route to get info from ny times news website
+  app.get("/scrape",function(req,res){
+
+    var count = 0; 
+
+    // First, we grab the body of the html with axios
+    axios.get("https://www.nytimes.com/").then(function(response){
+      // Then, load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+
+      //grab every h2 within an article tag
+      $("article a").each(function(i,element){    
+        console.log("i",i);
+        var result = {};
+        // Add the text and href of every link, and save them as properties of the result object
+
+        result.headline = $(this)
+        //   .siblings("div")
+          .children()
+          .text();
+        result.summary = $(this)
+          .children("p")
+          .text();
+          
+        result.url = $(this)
+        //   .parent("a")
+          .attr("href");
+
+        if ( ! result.url.includes("http"))  {  
+          result.url = "https:/www.nytimes.com" + result.url;
+        };    
+
+        if(result.title !== "" && result.summary !== "" && result.link !==""){  
+          count++;
+
+          console.log("result",result);
+          // Create a new news using the `result` object built from scraping
+          db.News.create(result)
+            .then(function(dbNews) {
+       
+                console.log(dbNews);
+            })
+            .catch(function(err) {
+              // If an error occurred, log it
+                console.log(err);
+            });
+        }
+      });
+      console.log("count",count);
+    });
+
+    
+  });
+
+
+
 };
